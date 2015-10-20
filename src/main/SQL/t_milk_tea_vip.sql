814d9294673d40186a23ed5cbb1de9db360efffc
huzehua <420681321@qq.com>
2015-10-20 22:15:00 +0800

    完成会员管理的功能

--- src/main/SQL/t_milk_tea_vip.sql
+++ src/main/SQL/t_milk_tea_vip.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50627
 File Encoding         : 65001
 
-Date: 2015-10-19 20:38:55
+Date: 2015-10-20 21:44:26
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,14 +20,15 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_milk_tea_vip`;
 CREATE TABLE `t_milk_tea_vip` (
-  `id` int(11) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varbinary(255) DEFAULT NULL,
   `tel` varbinary(13) DEFAULT NULL,
   `discount` decimal(10,0) DEFAULT NULL COMMENT '折扣',
   `integral` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_milk_tea_vip
 -- ----------------------------
+INSERT INTO `t_milk_tea_vip` VALUES ('1', 0x646173, 0x3131, '1', '1');
